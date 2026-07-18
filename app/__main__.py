3efe4faac77f0272fac9f687249b80978709255e
thekhoo <khoojinnwei@gmail.com>
2026-07-18 22:42:35 +0100

    feat: memray

--- app/__main__.py
+++ app/__main__.py
@@ -1,6 +1,8 @@
 import logging
 import sys
 
+from memray import Tracker
+
 from app.runner import run_simulation
 from app.utils.environment import EnvironmentManager
 
@@ -9,9 +11,16 @@ logger = logging.getLogger(__name__)
 
 def main() -> None:
     try:
-        with EnvironmentManager(log_level=logging.INFO) as env_manager:
+        with (
+            EnvironmentManager(log_level=logging.INFO) as env_manager,
+            Tracker(
+                file_name="memory_profile.bin",
+                native_traces=True,
+                trace_python_allocators=True,
+            ) as tracker,
+        ):
             config = env_manager.config
-            run_simulation(config)
+            run_simulation(config, tracker)
 
     except Exception as e:
         logger.error("An error occurred: %s", e)
--- app/runner.py
+++ app/runner.py
@@ -1,11 +1,18 @@
 import logging
+import time
+
+import memray
 
 from app.utils.config import RunnerConfig
 
 logger = logging.getLogger(__name__)
 
 
-def run_simulation(config: RunnerConfig) -> None:
+def make_blob(size_mb: int):
+    return bytearray(size_mb * 1024 * 1024)
+
+
+def run_simulation(config: RunnerConfig, tracker: memray.Tracker) -> None:
     cmd = [
         "Rscript",
         config.entrypoint,
@@ -17,5 +24,12 @@ def run_simulation(config: RunnerConfig) -> None:
     logger.info("Running simulation: %s", config.entrypoint)
     logger.debug("Command: %s", cmd)
     # result = subprocess.run(cmd, check=True, capture_output=True, text=True)
+
+    for i in range(10):
+        logger.info("Simulating work... %d/10", i + 1)
+        blob = make_blob(10)  # Simulate creating a 10 MB blob
+        logger.debug("Created a blob of size: %d bytes", len(blob))
+        time.sleep(10)  # Simulate time taken for processing
+
     logger.info("Simulation complete: %s", config.entrypoint)
     return None
